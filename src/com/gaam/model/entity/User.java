95c6ee0b5f3ae2ca5fa40b8810756019b4a9f3f1
efigueiro <everson.figueiro@gmail.com>
2013-10-02 23:28:01 -0300

    Adicionado projeto

---
+++ src/com/gaam/model/entity/User.java
@@ -0,0 +1,14 @@
+package com.gaam.model.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
